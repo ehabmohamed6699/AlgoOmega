85b3a44bc96b04c4763b016c56fd295840327b8f
Ehab Mohamed <ihabmohamed6699@gmail.com>
2022-03-11 01:04:53 +0200

    Uploaded first files of the project which are enough for it to work

---
+++ script.js
@@ -0,0 +1,22 @@
+let boxes = document.getElementsByClassName("box");
+let soon = document.getElementsByClassName("soon");
+let videoSrc = ["./videos/Lecture 1 - Part 1.m4v"];
+
+for (let i = 0; i < boxes.length; i++) {
+  boxes[i].onclick = function () {
+    document.querySelector(".play").classList.toggle("closed");
+    document
+      .querySelector(".play")
+      .querySelector("video").src = `${videoSrc[i]}`;
+  };
+}
+for (let i = 0; i < soon.length; i++) {
+  soon[i].onclick = function () {
+    window.alert("Video Will Be Avilable Soon");
+  };
+}
+
+document.getElementById("close-play").onclick = function () {
+  document.querySelector(".play").classList.toggle("closed");
+  document.querySelector(".play").querySelector("video").src = "";
+};
