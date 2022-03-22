bcb3e73633bcc6d868a40092c2ddd2ae78f886a6
Ehab Mohamed <ihabmohamed6699@gmail.com>
2022-03-22 22:05:29 +0200

    Uploaded Lecture 4 Part 2

--- script.js
+++ script.js
@@ -9,6 +9,7 @@ let videoSrc = [
   "./videos/Lecture 3 - Part 1.mp4",
   "./videos/Lecture 3 - Part 2.m4v",
   "./videos/Lecture 4 - Part 1.m4v",
+  "./videos/Lecture 4 - Part 2.m4v",
 ];
 
 for (let i = 0; i < boxes.length; i++) {
